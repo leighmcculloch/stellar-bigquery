6032f14e1e3faceddd7496e69a8b19df4b0ec970
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-08-16 02:05:30 +1000

    fix

--- queries/milestone_100m_contract_invocation.sql
+++ queries/milestone_100m_contract_invocation.sql
@@ -1,9 +1,15 @@
 -- Query to find the 100 millionth contract invocation.
 
+WITH txs AS
+(
+  SELECT
+    transaction_hash,
+    row_number() over (ORDER BY id) AS row_num
+  FROM `crypto-stellar.crypto_stellar.history_transactions`
+  WHERE operation_count = 1
+  AND soroban_resources_instructions > 0
+  AND successful = TRUE
+)
 SELECT transaction_hash
-FROM `crypto-stellar.crypto_stellar.history_transactions`
-WHERE operation_count = 1
-AND soroban_resources_instructions > 0
-AND successful = true
-LIMIT 1
-OFFSET 100000000
+FROM txs
+WHERE row_num = 100000000
